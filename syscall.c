1d57755ee21778c9c127048f05418ce9dc2e41bb
huoxinghuilai <lad-4@163.com>
2017-03-31 18:37:35 +0800

    xitongdiaoyongmoni

---
+++ syscall.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include"sys.h"
+
+//定义了两个内联的系统调用函数
+static inline systemcall(int,fork,nr_fork)
+static inline systemcall(int,exit,nr_exit)
+
+int main(){
+if(fork()==1)
+printf("fork成功\n");
+else
+printf("fork失败\n");
+if(exit()==0)
+printf("exit成功\n");
+else
+printf("exit失败\n");
+}
