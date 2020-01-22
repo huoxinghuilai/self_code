85143c79f0caaa36fe8e14fac9290b8e81bc7bd6
huoxinghuilai <lad-4@163.com>
2020-01-22 12:57:13 +0800

    冒泡排序

---
+++ test/mp_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float *sort(float A[])
+{
+	int i = 0, j = 0, t;
+	for (i; i < 10; i++)
+	{
+		for(j = i; j < 10; j++)
+		{
+			if(A[i] < A[j])
+			{
+				t = A[i];
+				A[i] = A[j];
+				A[j] = t;
+			}
+		}
+	}
+	return A;
+}
+int main()
+{
+	float A[10];
+	int i = 0;
+	for(i; i<11; i++)
+		A[i] = rand();
+	for(i = 0; i < 10; i++)
+		printf("%d ", A[i]);
+	printf("\n");
+	sort(A);
+	for(i = 0; i < 10; i++)
+		printf("%d ", A[i]);
+	printf("\n");
+	return 0;
+}
